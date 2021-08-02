14b76b18df42b9a034da264ae4c5c028946ca2db
Kelvyh <66328469+Kelvyh@users.noreply.github.com>
2021-08-02 10:02:17 -0300

    destrutor e insert

--- matriz.cpp
+++ matriz.cpp
@@ -25,6 +25,34 @@ Matriz::Matriz(int linhas, int colunas) {
     aux->direita = head;
 }
 
+void Matriz::insert(int i, int j, double value) {
+        //impossível inserir fora da matriz
+        if(i > Ltam || j > Ctam)
+            return;
+        //cria o novo nó e insere ele na coluna correta
+        Node *aux = head->direita;
+        while(aux->coluna != j)
+            aux = aux->direita;
+        //cout << "morreu";
+        Node *it = aux;
+        while(it->abaixo->linha != -1 && it->abaixo->linha < i)
+            it = it->abaixo;
+        Node *novo = new Node(i, j, value, nullptr, it->abaixo); //nodes???????
+        //cout << it->linha << "->" << novo->linha << "->" << it->abaixo->linha << endl;
+        it->abaixo = novo;
+        
+        //coloca o novo nó na linha correta
+        aux = head->abaixo;
+        while(aux->linha != i)
+            aux = aux->abaixo;;
+        it = aux;
+        while(it->direita->coluna != -1 && it->direita->coluna < j)
+            it = it->direita;
+        novo->direita = it->direita;
+        //cout << it->coluna << "->" << novo->coluna << "->" << it->direita->coluna << endl;
+        it->direita = novo;
+    }
+
 int Matriz::getTamLinhas() {
     return Ltam;
 }
@@ -38,5 +66,22 @@ Node* Matriz::getHead() {
 }
 
 Matriz::~Matriz() {
-    std::cout << "apagou";
+    Node *it_heads = head->direita;
+        Node *it_elems;
+        for(int j = 0; j < Ctam; j++) { //deleta as colunas e os elementos armazenados na matriz
+            it_elems = it_heads->abaixo;
+            while(it_elems != it_heads) {
+                Node *aux = it_elems;
+                it_elems = it_elems->abaixo;
+                delete aux;
+            }
+            it_heads = it_heads->direita;
+            delete it_elems;
+        }
+        it_heads = head->abaixo;
+        for(int i = 0; i < Ltam; i++) { //deleta as linhas
+            it_elems = it_heads;
+            it_heads = it_heads->abaixo;
+            delete it_elems;
+        }
 }
