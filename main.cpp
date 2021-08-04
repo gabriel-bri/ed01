701e2c630e236e18413066643c74a62d9f6ca8b6
Gabriel Brito <34654875+gabriel-bri@users.noreply.github.com>
2021-08-04 16:10:18 -0400

    Atualiza a main.cpp

--- main.cpp
+++ main.cpp
@@ -19,54 +19,30 @@ int main() {
 			tokens.push_back(buffer);
 		}
         
-		if(tokens[0] == "libera" || tokens[0] == "exit") {
-			for(int i = 0; i < matrizes.size(); ++i) {
-				delete matrizes[i];
-				matrizes[i] = nullptr;
-			}
-			matrizes.clear();
-			break;
-		}
-		// cria [l] [c]
 		if(tokens[0] == "cria") {
             string arq = tokens[1];
             Matriz *m = lerMatrizDeArquivo(arq);
 			matrizes.push_back(m);
 		}
-		// imprime [k]
+
 		else if(tokens[0] == "imprime") {
 			int k = stoi(tokens[1]);
 			matrizes[k]->print();
 		}
-		// nlinhas [k]
-		/*else if(tokens[0] == "nlinhas") {
-			int k = std::stoi(tokens[1]);
-			cout << "linhas: " << matrizes[k]->linhas() << endl;
-		}*/
-        // ncolunas [k]
-		/*else if(tokens[0] == "ncolunas") {
-			int k = std::stoi(tokens[1]);
-			cout << "colunas: " << matrizes[k]->colunas() << endl;
-		}*/
-        // valor [i] [j] [k]
-		/*else if(tokens[0] == "valor") {
-			int k = std::stoi(tokens[3]);
-            int i = std::stoi(tokens[1]);
-            int j = std::stoi(tokens[2]);
-			cout << "valor: " << matrizes[k]->valor(i,j) << endl;
-		}*/
+
 		// soma [p] [q] 
 		else if(tokens[0] == "soma") {
             int fst = std::stoi(tokens[1]);
             int snd = std::stoi(tokens[2]);
 			Matriz *C = soma(matrizes[fst],matrizes[snd]);
 			if(C == nullptr) 
-                cout << "nao foi possivel somar" << endl;
+                cout << "nao foi possivel somar\n" << endl;
             else {
                 C->print();
                 delete C;
             }
 		}
+
         // multiplica [p] [q] 
 		else if(tokens[0] == "multiplica") {
             int fst = std::stoi(tokens[1]);
@@ -79,8 +55,26 @@ int main() {
                 delete C;
             }
 		}
+
+		else if(tokens[0] == "libera" || tokens[0] == "sair") {
+			for(int i = 0; i < matrizes.size(); ++i) {
+				delete matrizes[i];
+				matrizes[i] = nullptr;
+			}
+			matrizes.clear();
+			break;
+
+			if(tokens[0] == "libera") {
+				cout << "Matrizes limpas\n";
+			}
+
+			if(tokens[0] == "sair") {
+				exit(1);
+			}
+		}
+
 		else {
-			cout << "comando inexistente" << endl;
+			cout << "Comando nao encontrado! \n";
 		}
 	}
 	return 0;
