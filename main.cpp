6368ce4699f75e4056db1e4b04f4effa97c1562a
Kelvyh <66328469+Kelvyh@users.noreply.github.com>
2021-08-05 11:15:08 -0300

    finalizada

--- main.cpp
+++ main.cpp
@@ -11,70 +11,79 @@ int main() {
 		string cmd;
 		getline(cin, cmd);
 
-		std::stringstream ss{ cmd };
+		stringstream ss{ cmd };
 		vector<string> tokens;
 		string buffer;
 
 		while(ss >> buffer) {
 			tokens.push_back(buffer);
 		}
-        
-		if(tokens[0] == "cria") {
-            string arq = tokens[1];
-            Matriz *m = lerMatrizDeArquivo(arq);
-			matrizes.push_back(m);
-		}
-
-		else if(tokens[0] == "imprime") {
-			int k = stoi(tokens[1]);
-			matrizes[k]->print();
-		}
-
-		// soma [p] [q] 
-		else if(tokens[0] == "soma") {
-            int fst = std::stoi(tokens[1]);
-            int snd = std::stoi(tokens[2]);
-			Matriz *C = soma(matrizes[fst],matrizes[snd]);
-			if(C == nullptr) 
-                cout << "nao foi possivel somar\n" << endl;
-            else {
-                C->print();
-                delete C;
-            }
-		}
-
-        // multiplica [p] [q] 
-		else if(tokens[0] == "multiplica") {
-            int fst = std::stoi(tokens[1]);
-            int snd = std::stoi(tokens[2]);
-			Matriz *C = multiplica(matrizes[fst],matrizes[snd]);
-			if(C == nullptr) 
-                cout << "nao foi possivel multiplicar" << endl;
-            else {
-                C->print();
-                delete C;
-            }
-		}
-
-		else if(tokens[0] == "libera" || tokens[0] == "sair") {
+        // finaliza o programa e/ou limpa as matrizes armazenadas
+		if(tokens[0] == "sair" || tokens[0] == "libera") {
 			for(int i = 0; i < matrizes.size(); ++i) {
 				delete matrizes[i];
 				matrizes[i] = nullptr;
 			}
 			matrizes.clear();
-			break;
-
-			if(tokens[0] == "libera") {
-				cout << "Matrizes limpas\n";
-			}
-
-			if(tokens[0] == "sair") {
-				exit(1);
-			}
+			if(tokens[0] == "sair")
+				break;
+			cout << "matrizes limpas" << endl;
 		}
-
+		// cria <nome_do_arquivo>
+		else if(tokens[0] == "cria") {
+            string arq = tokens[1];
+            Matriz *m = lerMatrizDeArquivo(arq);
+			matrizes.push_back(m);
+		}
+		// insere <valor> [i] [j] [k]
+		else if(tokens[0] == "insere") {
+			double valor = stod(tokens[1]);
+			int i = stoi(tokens[2]);
+            int j = stoi(tokens[3]);
+			int k = stoi(tokens[4]);
+			matrizes[k]->insert(i,j,valor);
+		}
+		// imprime [k]
+		else if(tokens[0] == "imprime") {
+			int k = stoi(tokens[1]);
+			matrizes[k]->print();
+		}
+        // valor [i] [j] [k]
+		else if(tokens[0] == "valor") {
+            int i = stoi(tokens[1]);
+            int j = stoi(tokens[2]);
+			int k = stoi(tokens[3]);
+			if(!matrizes[k]->naMatriz(i,j))
+				cout << "fora das dimensões da matriz" << endl;
+			else
+				cout << "valor: " << matrizes[k]->getValue(i,j) << endl;
+		}
+		// soma [fst] [snd] 
+		else if(tokens[0] == "soma") {
+            int fst = stoi(tokens[1]);
+            int snd = stoi(tokens[2]);
+			Matriz *C = soma(matrizes[fst],matrizes[snd]);
+			if(C == nullptr) 
+                cout << "nao foi possivel somar" << endl;
+            else {
+                C->print();
+                matrizes.push_back(C);
+            }
+		}
+        // multiplica [fst] [snd] 
+		else if(tokens[0] == "multiplica") {
+            int fst = stoi(tokens[1]);
+            int snd = stoi(tokens[2]);
+			Matriz *C = multiplica(matrizes[fst],matrizes[snd]);
+			if(C == nullptr) 
+                cout << "nao foi possivel multiplicar" << endl;
+            else {
+                C->print();
+                matrizes.push_back(C);
+            }
+		}
 		else {
-			cout << "Comando nao encontrado! \n";
+			cout << "comando inexistente" << endl;
 		}
 	}
 	return 0;
