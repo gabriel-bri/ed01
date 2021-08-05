0ac38248309b8f7d8870c66180dc913a3db6924c
Kelvyh <66328469+Kelvyh@users.noreply.github.com>
2021-08-05 11:14:41 -0300

    agora sim

--- Projeto.h
+++ Projeto.h
@@ -28,10 +28,12 @@ public:
     Matriz(int m,int n);
     //Destrutor que libera toda a memória que foi alocada dinamicamente para esta estrutura de dados.
     ~Matriz();
+    //Checa se as posições passada estão dentro da matriz
+    bool naMatriz(int i, int j);
     //Insere um valor na célula (i, j) da matriz, onde i é alinha e j é a coluna.
     void insert(int i,int j,double valor);
     //Devolve o valor na célula (i, j) da matriz, onde i é a linha e j é a coluna.
-    double getValue(int,int);
+    double getValue(int i,int j);
     //Devolve a quantidade de linhas da matriz
     int getTamLinhas();
     //Devolve a quantidade de colunas da matriz
